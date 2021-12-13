680925d862eadf51bec41ac37989822f9cdd0559
Konstantinos Liakopoulos <liakopoulos@olympusmarinegroup.com>
2021-12-13 16:36:39 +0200

    crud

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import React, { useReducer } from 'react';
 import { Button } from 'react-bootstrap';
 import ToDoList from './ToDoList'
+import { v4 as uuidv4 } from 'uuid';
 
 const todosInitialState = {
-  todos:[{ id:1, text: "finishing writing hooks chapter"},
-  { id:2, text: "play with kids"},
-  { id:3, text: "read bible"}
+  todos: [{ id: 1, text: "finishing writing hooks chapter" },
+  { id: 2, text: "play with kids" },
+  { id: 3, text: "read bible" }
   ]
 };
 
 export const TodosContext = React.createContext();
 
 function App() {
-  const [state, dispatch] = useReducer(todosReducer,todosInitialState)
+  const [state, dispatch] = useReducer(todosReducer, todosInitialState)
   return (
-<TodosContext.Provider value={{state,dispatch}}>
-<ToDoList />
-</TodosContext.Provider>
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <ToDoList />
+    </TodosContext.Provider>
   )
 }
 
-function todosReducer(state, action){
-  switch(action.type){
-      case 'delete':
-      const filteredTodoState = state.todos.filter( todo => todo.id !==
-      action.payload.id)
-      return {...state, todos: filteredTodoState}
-  default:
-  return todosInitialState
+function todosReducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      const newToDo = { id: uuidv4(), text: action.payload }
+      // add new todo onto array
+      const addedToDos = [...state.todos, newToDo]
+      // spread our state and assign todos
+      return { ...state, todos: addedToDos }
+    case 'delete':
+      const filteredTodoState = state.todos.filter(todo => todo.id !==
+        action.payload.id)
+      return { ...state, todos: filteredTodoState }
+    case 'edit':
+      const updatedToDo = { ...action.payload }
+      const updatedToDoIndex = state.todos.findIndex(t => t.id ===
+        action.payload.id)
+      const updatedToDos = [
+        ...state.todos.slice(0, updatedToDoIndex),
+        updatedToDo,
+        ...state.todos.slice(updatedToDoIndex + 1)
+      ];
+      return { ...state, todos: updatedToDos }
+    default:
+      return todosInitialState
   }
-  }
+}
 
 export default App;
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -7,11 +7,23 @@ function ToDoList() {
     // receive state and dispatch from index.js
     const { state, dispatch } = useContext(TodosContext);
     const [todoText, setTodoText] = useState("");
+    const [editMode, setEditMode] = useState(false)
+    const [editTodo, setEditTodo] = useState(null)
+    const buttonTitle = editMode ? "Edit" : "Add";
 
 
     const handleSubmit = event => {
         event.preventDefault();
+        if (editMode) {
+            dispatch({ type: 'edit', payload: { ...editTodo, text: todoText } })
+            setEditMode(false)
+            setEditTodo(null)
         }
+        else {
+            dispatch({ type: 'add', payload: todoText })
+        }
+        setTodoText("")
+    }
 
     return (
         <div>
@@ -23,7 +35,7 @@ function ToDoList() {
                         onChange={event => setTodoText(event.target.value)} />
                 </Form.Group>
                 <Button variant="primary" type="submit">
-                    Submit
+                    {buttonTitle}
                 </Button>
             </Form>
             <Table striped bordered hover>
@@ -38,7 +50,13 @@ function ToDoList() {
                     {state.todos.map(todo => (
                         <tr key={todo.id}>
                             <td>{todo.text}</td>
-                            <td>Edit</td>
+                            <td onClick={() => {
+                                setTodoText(todo.text)
+                                setEditMode(true)
+                                setEditTodo(todo)
+                            }}>
+                                Edit
+                            </td>
                             <td onClick={() =>
                                 dispatch({ type: 'delete', payload: todo })}>
                                 Delete
